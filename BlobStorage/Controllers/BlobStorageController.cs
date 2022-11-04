7ddaa208af1f007db0777704b1c8126f9e9a0c45
LOPEZ DIEGO MANUEL <godiework@hotmail.com>
2022-11-04 11:38:14 -0300

    Ejemplo de cargar archivos a Azure

---
+++ BlobStorage/Controllers/BlobStorageController.cs
@@ -0,0 +1,38 @@
+﻿using Azure.Storage.Blobs;
+using BlobStorage.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlobStorage.Controllers
+{
+    public class BlobStorageController : Controller
+    {
+        public readonly BlobServiceClient _blob;
+
+        public BlobStorageController(BlobServiceClient blob)
+        {
+            _blob = blob;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<String> Cargar(IFormFile file)
+        {
+            String mensaje;
+            try
+            {
+                BlobContainerClient contenedor = _blob.GetBlobContainerClient("imagenes");
+                await contenedor.UploadBlobAsync(file.FileName, file.OpenReadStream());
+                mensaje = "Se Cargo Exitosamente";
+            }
+            catch (Exception ex)
+            {
+                mensaje = ex.Message;
+            }
+
+            return mensaje;
+        }
+    }
+}
